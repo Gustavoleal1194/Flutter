20f4b7015382c5015146e942cc65e2362368335c
Gustavo Leal <gulealmir4@gmail.com>
2024-05-15 14:08:29 -0300

    flutterstudy

---
+++ lib/meu_aplicativo.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:testeflutter/pages/moedas_page.dart';
+
+class MeuAplicativo extends StatelessWidget {
+  const MeuAplicativo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Moedasbase',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: false),
+      home: const MoedasPage(),
+    );
+  }
+}
---
+++ lib/pages/moedas_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:testeflutter/repositories/moeda_repository.dart';
+
+class MoedasPage extends StatefulWidget {
+  const MoedasPage({super.key});
+
+  @override
+  State<MoedasPage> createState() => _MoedasPageState();
+}
+
+class _MoedasPageState extends State<MoedasPage> {
+  @override
+  Widget build(BuildContext context) {
+    final tabela = MoedaRepository.tabela;
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Cripto Moedas'),
+        ),
+        body: ListView.separated(
+          padding: const EdgeInsets.all(16),
+          itemBuilder: (BuildContext context, int moeda) {
+            return ListTile(
+              leading: Image.asset(tabela[moeda].icone),
+              title: Text(tabela[moeda].nome),
+              trailing: Text(tabela[moeda].preco.toString()),
+            );
+          },
+          separatorBuilder: (_, __) => const Divider(),
+          itemCount: tabela.length,
+        ));
+  }
+}
