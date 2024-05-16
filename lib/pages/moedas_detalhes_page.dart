588bf1c71f9bdcc559b619c85e84dfcec1d5b9f4
Gustavo Leal <gulealmir4@gmail.com>
2024-05-16 18:01:58 -0300

    estudos flutter1605

---
+++ lib/pages/moedas_detalhes_page.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
+import 'package:testeflutter/models/moeda.dart';
+
+class MoedasDetalhesPage extends StatefulWidget {
+  Moeda moeda;
+
+  MoedasDetalhesPage({super.key, required this.moeda});
+
+  @override
+  State<MoedasDetalhesPage> createState() => _MoedasDetalhesPageState();
+}
+
+class _MoedasDetalhesPageState extends State<MoedasDetalhesPage> {
+  NumberFormat real = NumberFormat.currency(locale: 'pt_br', name: 'R\$');
+  final _form = GlobalKey<FormState>();
+  final _valor = TextEditingController();
+  double quantidade = 0;
+  comprar() {
+    if (_form.currentState!.validate()) {
+      //salvar a compra
+
+      Navigator.pop(context);
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Compra realizada com sucesso!'),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.moeda.nome),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 24),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 50,
+                  child: Image.asset(widget.moeda.icone),
+                ),
+                Container(width: 10),
+                Text(
+                  real.format(widget.moeda.preco),
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: -1,
+                    color: Colors.grey[800],
+                  ),
+                )
+              ],
+            ),
+          ),
+          (quantidade > 0)
+              ? SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: Container(
+                    margin: const EdgeInsets.only(bottom: 24),
+                    padding: const EdgeInsets.all(12),
+                    alignment: Alignment.center,
+                    decoration:
+                        BoxDecoration(color: Colors.teal.withOpacity(0.05)),
+                    child: Text(
+                      '$quantidade ${widget.moeda.sigla}',
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.teal,
+                      ),
+                    ),
+                  ),
+                )
+              : Container(margin: const EdgeInsets.only(bottom: 24)),
+          Form(
+            key: _form,
+            child: TextFormField(
+              controller: _valor,
+              style: const TextStyle(fontSize: 22),
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'valor',
+                prefixIcon: Icon(Icons.monetization_on_outlined),
+                suffix: Text(
+                  'R\$',
+                  style: TextStyle(fontSize: 14),
+                ),
+              ),
+              keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'informe o valor da compra';
+                } else if (double.parse(value) < 50) {
+                  return 'compra minima é 50 R\$';
+                }
+                return null;
+              },
+              onChanged: (value) {
+                setState(() {
+                  quantidade = (value.isEmpty)
+                      ? 0
+                      : double.parse(value) / widget.moeda.preco;
+                });
+              },
+            ),
+          ),
+          Container(
+            alignment: Alignment.bottomCenter,
+            margin: const EdgeInsets.only(top: 24),
+            child: ElevatedButton(
+              onPressed: comprar,
+              child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.check),
+                    Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Text(
+                          'Comprar',
+                          style: TextStyle(fontSize: 20),
+                        ))
+                  ]),
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+}
--- lib/pages/moedas_page.dart
+++ lib/pages/moedas_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:testeflutter/pages/moedas_detalhes_page.dart';
 import 'package:testeflutter/repositories/moeda_repository.dart';
 import 'package:intl/intl.dart';
 import 'package:testeflutter/models/moeda.dart';
@@ -24,56 +25,86 @@ class _MoedasPageState extends State<MoedasPage> {
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           onPressed: () {
-            setState(() {
-              selecionadas = [];
-            });
+            setState(
+              () {
+                selecionadas = [];
+              },
+            );
           },
         ),
         title: Text('${selecionadas.length} selecionadas'),
+        backgroundColor: Colors.blueGrey[50],
+        elevation: 1,
+        iconTheme: const IconThemeData(color: Colors.black87),
+        titleTextStyle: const TextStyle(
+          color: Colors.black87,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
       );
     }
+  }
+
+  mostrarDetalhes(Moeda moeda) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => MoedasDetalhesPage(moeda: moeda),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: appBarDinamica(),
-        body: ListView.separated(
-          padding: const EdgeInsets.all(16),
-          itemBuilder: (BuildContext context, int moeda) {
-            return ListTile(
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(12)),
-              ),
-              leading: (selecionadas.contains(tabela[moeda]))
-                  ? const CircleAvatar(
-                      child: Icon(Icons.check),
-                    )
-                  : SizedBox(
-                      width: 40,
-                      child: Image.asset(tabela[moeda].icone),
-                    ),
-              title: Text(tabela[moeda].nome,
-                  style: const TextStyle(
-                    fontSize: 17,
-                    fontWeight: FontWeight.w500,
-                  )),
-              trailing: Text(
-                real.format(tabela[moeda].preco),
-              ),
-              selected: selecionadas.contains(tabela[moeda]),
-              selectedTileColor: Colors.indigo[50],
-              onLongPress: () {
-                setState(() {
-                  (selecionadas.contains(tabela[moeda]))
-                      ? selecionadas.remove(tabela[moeda])
-                      : selecionadas.add(tabela[moeda]);
-                });
-              },
-            );
-          },
-          separatorBuilder: (_, __) => const Divider(),
-          itemCount: tabela.length,
-        ));
+      appBar: appBarDinamica(),
+      body: ListView.separated(
+        padding: const EdgeInsets.all(16),
+        itemBuilder: (BuildContext context, int moeda) {
+          return ListTile(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(12)),
+            ),
+            leading: (selecionadas.contains(tabela[moeda]))
+                ? const CircleAvatar(
+                    child: Icon(Icons.check),
+                  )
+                : SizedBox(
+                    width: 40,
+                    child: Image.asset(tabela[moeda].icone),
+                  ),
+            title: Text(tabela[moeda].nome,
+                style: const TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w500,
+                )),
+            trailing: Text(
+              real.format(tabela[moeda].preco),
+            ),
+            selected: selecionadas.contains(tabela[moeda]),
+            selectedTileColor: Colors.indigo[50],
+            onLongPress: () {
+              setState(() {
+                (selecionadas.contains(tabela[moeda]))
+                    ? selecionadas.remove(tabela[moeda])
+                    : selecionadas.add(tabela[moeda]);
+              });
+            },
+            onTap: () => mostrarDetalhes(tabela[moeda]),
+          );
+        },
+        separatorBuilder: (_, __) => const Divider(),
+        itemCount: tabela.length,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: selecionadas.isNotEmpty
+          ? FloatingActionButton.extended(
+              onPressed: () {},
+              icon: const Icon(Icons.star),
+              label: const Text('FAVORITAR',
+                  style:
+                      TextStyle(letterSpacing: 0, fontWeight: FontWeight.bold)))
+          : null,
+    );
   }
 }
